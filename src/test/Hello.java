d809cbf4ba390e478936a37e807ca17f602ad1af
Raul Marks <raul.marks@itcollege.ee>
2012-12-16 15:21:00 +0200

    test commit

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -10,7 +10,7 @@ public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().print("Hello!");
+		response.getWriter().print("Hello-hello!");
 	}
 
 }
